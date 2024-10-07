e9819abf94fa10f52bc4bd6457815626507d048d
RohanS00007 <rohansingh70112@gmail.com>
2024-10-07 20:05:17 +0530

    Add files via upload

---
+++ pascal.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>generaterow(int row){
+        long long ans=1;
+        vector<int>ansrow;
+        ansrow.push_back(1);
+        for(int i=1;i<row;i++){
+            ans=ans*(row-i);
+            ans=ans/(i);
+            ansrow.push_back(ans);
+        }
+        return ansrow;
+
+    }
+
+vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>>v;
+        for(int i=1;i<=numRows;i++){
+            v.push_back(generaterow(i));
+        }
+        return v;
+    }
